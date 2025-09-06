d00dda1725c0ffcb288aaffd91d4274c8fb24803
Sweta Kumari <112224398+swetamishra1611@users.noreply.github.com>
2025-09-06 19:47:18 +0000

    till one hr

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,23 @@
+const axios  = require("axios");
+
 function sum(a,b){
     return a+b
 }
 
+const  BACKEND_URL = "http://localhost:3000"
+
 describe("Authentication",() => {
 
-test('User is able to sign up', () => {
+  test('User is able to sign up', async() => {
+    const username = "kirat" + Math.random();
+    const password ="123456";
+    const response = await axios.post(' $ {BACKEND_URL}/api/v1/signup',{
+        username,
+        password
+
+    })
+    
+    expect(response.statusCode).toBe(200)
     
     });
 
