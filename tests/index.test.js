5f40a037448fae0515b9c238732a7b43ba77bcbe
Sweta Kumari <112224398+swetamishra1611@users.noreply.github.com>
2025-09-07 18:29:25 +0000

    Till (8:15)

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,20 +7,56 @@ function sum(a,b){
 const  BACKEND_URL = "http://localhost:3000"
 
 describe("Authentication",() => {
-
-  test('User is able to sign up', async() => {
-    const username = "kirat" + Math.random();
-    const password ="123456";
-    const response = await axios.post(' $ {BACKEND_URL}/api/v1/signup',{
+    test('User is able to sign up only once', async() => {
+      const username = "kirat" + Math.random();
+      const password ="123456";
+      const response = await axios.post(' ${BACKEND_URL}/api/v1/signup',{
         username,
-        password
+        password,
+        type: "admin"
 
     })
     
     expect(response.statusCode).toBe(200)
+    const updatedResponse = await axios.post('${BACKEND_URL}/api/v1/signup',{
+        username,
+        password,
+        type:"admin"
+    })
+    expect(updatedResponse.statusCode).toBe(400);
     
     });
 
+    test('signup request fails if the username is empty',async()=> {
+        const usernanme = 'kirat- ${Math.random()}'
+        const password = "123456"
+
+        const response = await axios.post('${BACKEND_URL}/api/v1/signup',{
+            password
+        })
+
+
+        expect (response.statusCode).toBe(400)
+
+    })
+
+    test('Signin succeeds if the username  and password are correct',async()=> {
+        const usernanme = 'kirat- ${Math.random()}'
+        const password = "123456"
+
+        await axios.post('${BACKEND_URL}/api/v1/signup',{
+            username,
+            password
+        });
+
+        const response = await axios.post('${BACKEND_URL}/api/v1/signin', {
+            usernanme,
+            password
+        });
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.token).toBeDefined()
+
 })
 
-
+})
